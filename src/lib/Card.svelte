7bcf5e328ff625551dde3a41bc8a93df56b0a62f
pohadkove <80869110+pohadkove@users.noreply.github.com>
2022-08-20 19:41:37 -0700

    about me

---
+++ src/lib/Card.svelte
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+<div class="pf-card">
+    <div class="header">
+    <h1>Pohadkove</h1>
+    <img class="pfp" src="https://avatars.githubusercontent.com/u/80869110?v=4" alt="pfp">
+</div>
+<div class="divider"></div>
+<p>Insert a generic about me here idk...</p>
+</div>
+<style>
+    .divider{
+        background-color: #ffffff;
+        height: 2px;
+        width: 100%;
+    }
+    .pf-card{
+  background:var(--bg-color);
+  box-shadow: 0px 0px 20px 1px #0a0ab9;
+  border-radius: 5px;
+  padding: 1em;
+  flex-direction: column;
+}
+.pf-card h1{
+    text-align: center;
+    font-size: 2em;
+    margin: 0;
+    padding: 0;
+    margin-right: 1em;
+}
+.header{
+    display: flex;
+}
+.pfp{
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  display: block;
+     margin-left: auto;
+     border: #ffffff solid 2px;
+     transition: 0.3s;
+}
+.pfp:hover{
+     opacity: 60%;
+     cursor: pointer;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <script>
+	import { onMount } from 'svelte';
+import Card from '../lib/Card.svelte';
+	onMount( () => {
+
+  });
 </script>
 
 <div class="main">
@@ -11,8 +16,8 @@
 			<a href="/">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					width="25"
-					height="25"
+					width="35"
+					height="35"
 					fill="#ffffff"
 					class="bi bi-braces-asterisk"
 					viewBox="0 0 16 16"
@@ -21,7 +26,7 @@
 						fill-rule="evenodd"
 						d="M1.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C2.25 2 1.49 2.759 1.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6ZM14.886 7.9v.164c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456v-1.3c-1.114 0-1.49-.362-1.49-1.456V4.352C14.51 2.759 13.75 2 12.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6ZM7.5 11.5V9.207l-1.621 1.621-.707-.707L6.792 8.5H4.5v-1h2.293L5.172 5.879l.707-.707L7.5 6.792V4.5h1v2.293l1.621-1.621.707.707L9.208 7.5H11.5v1H9.207l1.621 1.621-.707.707L8.5 9.208V11.5h-1Z"
 					/>
-				</svg>
+					</svg>
 			</a>
 		</div>
 	</nav>
@@ -42,17 +47,46 @@
 			</svg>
 		</div>
 	</div>
-
 <div class="project-1">
+	<div>
 	<a class="glow" href="https://groovy-songs.vercel.app/" target="_blank"><h1>Groovy</h1></a>
 </div>
+	<div>
+	<p><a href="https://groovy-songs.vercel.app/" target="_blank">Groovy</a> is a site to discover new music it uses firebase on the backend to let users submit songs and to display songs</p>
+</div>
+</div>
 
+<div class="about">
+	<Card />
+</div>
 <style>
+		.about {
+			height: 100vh;
+		background: url(/about.jpg);
+		background-position: center;
+		background-size: cover;
+		display: flex;
+		align-items: center; /* Vertical center alignment */
+		justify-content: center; /* Horizontal center alignment */
+		background-attachment: fixed;
+		flex-direction: column;
+	}
 	.project-1 {
 		height: 100vh;
 		display: flex;
 		align-items: center; /* Vertical center alignment */
 		justify-content: center; /* Horizontal center alignment */
+		background-size: cover;
+		flex-direction: column;
+	}
+	.project-1 p{
+		font-size: 1.5em;
+		text-align: center;
+	}
+	.project-1 p a{
+		font-size: inherit;
+		color: inherit;
+		text-decoration: underline;
 	}
 	.nav {
 		position: fixed; /* Set the navbar to fixed position */
@@ -72,12 +106,19 @@
 		display: flex;
 		align-items: center; /* Vertical center alignment */
 		justify-content: center; /* Horizontal center alignment */
+		background-attachment: fixed;
 	}
 	a{
 		cursor: pointer;
 	}
+	a:hover{
+		opacity: 95%;
+	}
 	.pointer-defualt{
 		cursor:default;
+	}
+	nav .glow{
+		font-size: 2em;
 	}
 	.glow {
 		color: white;
